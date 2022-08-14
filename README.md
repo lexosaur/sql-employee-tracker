8caf9b68473fdbdeaf8729179a2ade716ac27b10
Alexis García <alexis.garciad@gmail.com>
2022-08-14 13:26:01 -0500

    commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node.js User Management System - Express, Express-Handlebars, HBS, MySQL
+
+This was created based on the Raddy YouTube Video tutorial.
+
+## Installation
+To run this project, install it locally using npm:
+
+```
+$ npm install
+$ npm start
+```
---
+++ user-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user` 
+(`id`, `first_name`,  `last_name`,    `email`,                 `phone`,         `comments`, `status`) VALUES
+(NULL, 'Alexis',      'García',       'alexis@email.com',      '012345 678910', '',          'active'),
+(NULL, 'Mark',        'Perez',        'mperez@email.com',      '012345 678910', '',          'active'),
